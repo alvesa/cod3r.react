e10e70cbe5b9fdbc449bb135d1d82d827eb27005
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2021-02-01 07:35:07 -0300

    Usando flex box para alinhar os cards e wrap para ajustar conforme tamanho da tela (tipo media query)

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
+import './App.css';
 
 export default _ => (
-  <div>
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Card titulo="Desafio Aleatorio">
-      <Aleatorio min={10} max={30} />
-    </Card>
-    <Card titulo="Fragmento">
-      <Fragmento />
-    </Card>
-    <Card titulo="Com Parametro">
-      <ComParametro titulo="Segundo componente" subtitulo="Andre" />
-    </Card>
-    <Card titulo="Primeiro Component">
-      <Primeiro />
-    </Card>
+    <div className="Cards">
+      <Card titulo="Desafio Aleatorio">
+        <Aleatorio min={10} max={30} />
+      </Card>
+      <Card titulo="Fragmento">
+        <Fragmento />
+      </Card>
+      <Card titulo="Com Parametro">
+        <ComParametro titulo="Segundo componente" subtitulo="Andre" />
+      </Card>
+      <Card titulo="Primeiro Component">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
