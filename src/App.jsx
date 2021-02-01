31be5361fc2877630998bbfe6e7d823800907fc0
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2021-02-01 07:24:55 -0300

    Colocando os itens dentro de cards utilizando a props.children

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/Card';
 
 export default _ => (
   <div>
     <h1>Fundamentos React</h1>
-    <Aleatorio min={10} max={30} />
-    <Fragmento />
-    <ComParametro titulo="Segundo componente" subtitulo="Andre" />
-    <Primeiro />
+    <Card titulo="Desafio Aleatorio">
+      <Aleatorio min={10} max={30} />
+    </Card>
+    <Card titulo="Fragmento">
+      <Fragmento />
+    </Card>
+    <Card titulo="Com Parametro">
+      <ComParametro titulo="Segundo componente" subtitulo="Andre" />
+    </Card>
+    <Card titulo="Primeiro Component">
+      <Primeiro />
+    </Card>
   </div>
 );
