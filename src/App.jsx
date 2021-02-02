5ba8d2f238f57c0bae91cbea7234448d80d283c3
Andre Euclides Alves <andreeuclidesalves@MacBook-Pro-de-Andre.local>
2021-02-02 06:56:32 -0300

    Passando as props de um componente via spread

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import './App.css';
+import Familia from './components/basicos/Familia';
 
 export default _ => (
   <div className="App">
     <h1>Fundamentos React</h1>
     <div className="Cards">
+      <Card titulo="Desafio Aleatorio" color="#00C8F8">
+        <Familia sobrenome="Ferreira">
+        </Familia>
+      </Card>
       <Card titulo="Desafio Aleatorio" color="#080">
         <Aleatorio min={10} max={30} />
       </Card>
